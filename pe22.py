f74ee2982007c9a8d04f46a4240295449f09975e
Eric H <20998658+ericthansen@users.noreply.github.com>
2019-01-14 13:22:39 -0600

    pe 22 commit

---
+++ pe22.py
@@ -0,0 +1,31 @@
+# Names scores
+# Problem 22 
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+# What is the total of all the name scores in the file?
+def nval(s):
+    d={'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
+    tot=0
+    for c in s:
+        tot+=d[c]
+    return tot
+#print(nval("ABCD"))
+
+def main():
+    t=[]
+    with open('p022_names.txt','r') as f:
+        for line in f:
+            for word in line.split(','):
+               t.append(word.strip('"'))
+               #print(word)  
+    #print(t)
+    t1=sorted(t)
+    #print(t1)
+    totscore=0
+    for i in range(len(t1)):
+        namescore=(i+1)*(nval(t1[i]))
+        totscore+=namescore
+    print(totscore)
+main()
