35c6d8da8b71f66dfc6ddd12ba8cbce752f90a26
Eric H <20998658+ericthansen@users.noreply.github.com>
2020-07-31 14:06:46 -0500

    Update pe67.py

--- pe67.py
+++ pe67.py
@@ -1,3 +1,5 @@
+# Problem Source: https://projecteuler.net/problem=67
+
 # Problem 67 
 # By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
